2d41d7aa137f6d04e7f3139708395bfb0b0c1d20
Stefan Rodrigues <stefanrodrigues@Stefans-MacBook-Air.local>
2022-07-16 15:20:04 +0100

    updated script with comments

--- basket.rb
+++ basket.rb
@@ -1,3 +1,4 @@
+#Inventory
 class Items
     include Enumerable 
     def each
@@ -7,21 +8,24 @@ class Items
     end
 end
 
+#Printing all the list of items available with pricing details
 puts "List of items available to buy"
 items_list = Items.new 
 items_list.each do |item|
     puts item
 end
 
-print "How many items would you like to buy: "
+#Accepting user input for number of items
+puts "How many items would you like to buy: "
 no_of_items = gets.to_i
-print "!!!BUY ONE GET ONE OFFER ON BAS!!\n!!DISCOUNTED RATE OF £4.50 on PAR if you buy 3 or more!!\n"
+puts "!!!BUY ONE GET ONE OFFER ON BAS!!\n!!DISCOUNTED RATE OF £4.50 on PAR if you buy 3 or more!!\n"
 
 bas_quantity = 0
 cor_quantity = 0
 par_quantity = 0
 iter = 1
-print "Enter item code: "
+puts "Enter item code: "
+#while loop to accept input and case to increment quantity for each item
 while iter <= no_of_items
     item_code = gets.chomp
     iter = iter+1
@@ -35,6 +39,7 @@ while iter <= no_of_items
     end
 end
 
+#function to calculate pricing rule for parsley
 def parsley_total(par_quantity)
     if par_quantity>=3
         tot_par = 4.5*par_quantity
@@ -44,6 +49,7 @@ def parsley_total(par_quantity)
     return tot_par
 end
 
+#function to calculate pricing rule for basil
 def basil_total(bas_quantity)
     if bas_quantity%2==0
         tot_basil = 3.11*(bas_quantity/2)
@@ -56,4 +62,5 @@ end
 basil_price = basil_total(bas_quantity)   
 parsley_price = parsley_total(par_quantity)
 
+#calculating basket total
 puts "Basket Total = £#{basil_price + parsley_price + 11.23*cor_quantity}"
